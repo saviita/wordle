12302c00c2bda63225238054a18f4b6afaa3895c
saviita <taatt2000@gmail.com>
2024-07-11 23:05:14 +0200

    11/07

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,17 @@ import '../scss/styles.scss';
 const formElement = document.getElementById('form')
 const inputElement = document.getElementById('input')
 const gameBoradElement = document.getElementById('gameboard')
+const rootStyles = document.documentElement.style
 const WORDS = ['rojo', 'casa', 'flores', 'botas', 'ojo', 'pez']
 const TRIESNUMBER = 5
 
 let secretWord;
-let rowCounter;
+let currentRow = 0;
+let correctWord = false;
 
 const generateLetters = (wordLength) => {
     const fragment = document.createDocumentFragment()
-    console.log(wordLength)
+
     for (let i = 0; i < TRIESNUMBER; i++) {
         const newRow = document.createElement('div')
         newRow.classList.add('game__row')
@@ -29,8 +31,6 @@ const generateLetters = (wordLength) => {
 
     gameBoradElement.append(fragment)
 }
-const row = document.getElementsByClassName('game__row')
-console.log(row)
 
 const randomPosition = () => {
     const randomNumber = Math.floor(Math.random() * WORDS.length)
@@ -40,22 +40,59 @@ const randomPosition = () => {
 }
 randomPosition()
 
-const checkLetters = () => {
+const paintLetter = (letter, position, className) {
+    const letterBox = gameBoradElement.children[currentRow].children[i]
+    letterBox.textContent = letter
 
+    if(!letterBox.classList.contains('green')) {
+        letterBox.classList.add
+    }
 }
 
-const insertWord = (value) => {
-    // console.log(row)
-    let counter = 0
-    for (let i = 0; i < secretWord.length; i++) {
-        row[counter].textContent = value.charAt(i)
+const checkRow = (userWord) => {
+    let className;
+    // if (correctWord) {
+    //     console.log('Palabra correcta')
+    //     return
+    // }
+    
+    for (let i = 0; i < userWord.length; i++) {
+        let letter = userWord[i]
+        let wordToCheck = secretWord
+
+        if (secretWord[i] === letter) {
+            className = 'green'
+            wordToCheck = wordToCheck.replace(letter, '-')
+            paintLetter(letter, i, className)
+        }
+
     }
+
+    for (let i = 0; i < userWord.length; i++) {
+        const letter = userWord[i]
+
+        if(wordToCheck.includes(letter)) {
+            className = 'orange'
+            wordToCheck = wordToCheck.replace(letter, '-')
+        } else {
+            className = 'gray'
+        }
+    }
+
+    secretWord === userWord ? correctWord = true : correctWord = false
+
+    currentRow++
 }
 
 const getWord = event => {
     event.preventDefault();
-    
-    const inputValue = inputElement.value
-    insertWord(inputValue)
+    const userWord = inputElement.value
+
+    if(userWord.length !== secretWord.length) {
+        console.log(`La palabra tiene que tener ${secretWord.length} letras`)
+    }
+
+    checkRow(userWord)
+    event.target.reset();
 }
 formElement.addEventListener('submit', getWord)
